85a0c27d7f0306a46d4e7a2e3e5398433b463953
Nikita Osipov <112751679+nikit01@users.noreply.github.com>
2023-06-23 22:45:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let offset = 0;
+const slaiderLine = document.querySelector('.slaider--line');
+document.querySelector('.btn--next').addEventListener('click', function(){
+    offset = offset + 1423;
+    if(offset > 5692){
+        offset = 0;
+    }
+    slaiderLine.style.left = -offset + 'px';
+});
+document.querySelector('.btn--prev').addEventListener('click', function(){
+    offset = offset - 1423;
+    if(offset < 0){
+        offset = 5692;
+    }
+    slaiderLine.style.left = -offset + 'px';
+});
+// 
+$('.slaider--line--media').slick()
+// 
+$('.collection--case--media').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
